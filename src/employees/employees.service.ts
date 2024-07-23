db72a72f330cc667c532d48659f21f1df13b7611
Martin Ganami <mganami@gmail.com>
2024-07-23 20:03:24 -0300

    feat(get-employees): adding new endpoint to get all active employees

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -4,22 +4,31 @@ export type Employee = any;
 
 @Injectable()
 export class EmployeesService {
+  private readonly ACTIVE_STATUS = 1;
   private readonly employees = [
     {
       id: '12345',
       name: 'Yanet',
       typeService: 'kidsCare',
       startDate: '03-03-2018',
+      status: 1,
     },
     {
       id: '67890',
       name: 'Antonia',
       typeService: 'cleanning',
       startDate: '03-03-2016',
+      status: 1,
     },
   ];
 
   async findOne(employeeId: string): Promise<Employee | undefined> {
     return this.employees.find((employee) => employee.id === employeeId);
+  }
+
+  async findAllActives(): Promise<Employee | []> {
+    return this.employees.filter(
+      (employee) => employee.status === this.ACTIVE_STATUS,
+    );
   }
 }
