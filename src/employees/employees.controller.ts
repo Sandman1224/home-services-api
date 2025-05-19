301fe76ee8149c4812a9c7141d18f93b2cc3d5c2
Martin Ganami <mganami@gmail.com>
2025-05-19 17:58:08 -0300

    Add endpoint and service logic to fetch employee by ID
    Introduced a new API endpoint to retrieve an employee by their ID. Updated the service layer to handle the logic with appropriate error handling for non-existent IDs. Added corresponding tests to verify the new functionality.

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 
 @Controller('employees')
@@ -8,5 +8,10 @@ export class EmployeesController {
   @Get()
   getActiveEmployees() {
     return this.employeesService.findAllActives();
+  }
+
+  @Get(':id')
+  getEmployeeById(@Param('id') employeeId: string) {
+    return this.employeesService.getEmployeeById(employeeId);
   }
 }
